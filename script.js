718941af68b0e632fb82264b0f38466e69f34347
MennyMo <molade779@gmail.com>
2021-12-30 10:59:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let navigation = document.querySelector('.navigation');
+let toggle = document.querySelector('.toggle');
+function toggleMenu(){
+    navigation.classList.toggle('active')
+    toggle.classList.toggle('active')
+}
